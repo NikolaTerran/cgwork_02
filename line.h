f0a5dd3c1e98c59137922a40b435970f1109405e
Nikola Terran <nikolaterranthe1@gmail.com>
2019-02-06 11:46:30 -0500

    S

---
+++ line.h
@@ -0,0 +1,3 @@
+int drawLine(int x1, int y1, int x2, int y2, int *color);
+void line_helper(int x1, int y1, int x2, int y2, int x3, int y3, int octants, int * color, int m);
+void initialize(int file, int * colour);
--- main.c
+++ main.c
@@ -3,7 +3,7 @@
 #include <string.h>
 #include <unistd.h>
 #include <fcntl.h>
-
+#include "line.h"
 
 int* sys_random(int size){
 	int *buffer;
@@ -19,59 +19,135 @@ int* sys_random(int size){
 	return buffer;
 }
 
+void line_helper(int x1, int y1, int x2, int y2, int x3, int y3, int octants, int * color, int m){
+	
+	double slope;
+	
+	while(x3 != x2 && y3 != y2){
+		switch(octants){
+			case 1: slope = (y3 - y1)/(x3 - x1);
+					if(slope >= m){
+						array[x3][y3][0] = color[0];
+						array[x3][y3][1] = color[1];
+						array[x3][y3][2] = color[2];
+						line_helper(x1,y1,x2,y2,x3 + 1,y3 + 1,color,m);
+					}else{
+						array[x3][y3 - 1][0] = color[0];
+						array[x3][y3 - 1][1] = color[1];
+						array[x3][y3 - 1][2] = color[2];
+						line_helper(x1,y1,x2,y2,x3 + 1,y3,color,m);
+					}		
+			break;
+			case 2: //up right
+			break;
+			case 3: //up left
+			break;
+			case 4: //left up
+			break;
+			case 5: //left down
+			break;
+			case 6: //down left
+			break;
+			case 7: //down right
+			break;
+			case 8: //right down
+			break;
+		}
+	}
+}
+
+int drawLine(int x1 , int y1, int x2, int y2, int * color){
+	int octants;
+	
+	double m = (y1 - y2) / (x2 - x1);
+	
+	if(y2 < y1){
+		if(x2 > x1){
+			if( m <= 1 ){
+			     octants = 1;
+			}else{
+			     octants = 2;
+			}
+		}else{
+			if( m <= -1 ){
+			     octants = 3;
+			}else{
+			     octants = 4;
+			}
+		}
+	}else{
+	   if(x2 < x1){
+			if( m <= 1 ){
+			     octants = 5;
+			}else{
+			     octants = 6;
+			}
+		}else{
+			if( m <= -1 ){
+			     octants = 7;
+			}else{
+			     octants = 8;
+			}
+		}
+	}
+	
+	array[x1][y1][0] = color[0];
+	array[x1][y1][1] = color[1];
+	array[x1][y1][2] = color[2];
+	switch(octants){
+			case 1: line_helper(x1,y1,x2,y2,x1 + 1,y1 + 1,color,m);
+					 
+			//right up
+			break;
+			case 2: //up right
+			break;
+			case 3: //up left
+			break;
+			case 4: //left up
+			break;
+			case 5: //left down
+			break;
+			case 6: //down left
+			break;
+			case 7: //down right
+			break;
+			case 8:// right down
+			break;
+			}
+}
+
+void initialize(int file, int * colour){
+
+	char line[20];
+	int i, j;
+	
+	snprintf(line, sizeof(line), "P3 500 500 255\n");
+	write (file, line, strlen(line));
+	
+	for(i=0; i < 500; i ++){
+		for(j=0; j < 500; j++){
+			snprintf(line, sizeof(line), "%d %d %d\n", colour[0], colour[1], colour[2]);
+			write(file, line, strlen(line));
+		}		 
+	}
+}
+
+
+
 int main(){
 
-int fd, i,j , k;
-char line[20];
+int fd, i, j , k;
 int rgb[3];
 
-int int_buffer;
+rgb[0] = 255;
+rgb[1] = 255;
+rgb[2] = 255;
 
-int size = 750000;
-int * buffer = sys_random(size);
+int array[500][500][3];
+array = malloc(sizeof(int) * 500 * 500 * 3);
 
 fd = open("image.ppm", O_CREAT | O_TRUNC | O_WRONLY, 0644);
-snprintf(line, sizeof(line), "P3 500 500 255\n");
-write (fd, line, strlen(line));
 
-/*
-FILE* entropy;
-entropy = fopen("/dev/urandom", "rb");
-printf("entropy: %d\n",entropy);
-*/
+initialize(fd, rgb);
 
-k = 0;
-
-int random1, random2, random3;
-
-	for(i=0; i<500; i ++){
-		for(j=0; j<500; j++){
-			printf("processing: %d/75000\n",k);
-	/*
-			random1 = fread(&int_buffer, sizeof(int), 1, entropy);
-			random2 = fread(&int_buffer, sizeof(int), 1, entropy);
-			random3 = fread(&int_buffer, sizeof(int), 1, entropy);
-	*/		
-			//printf("random1: %d\n",random1);
-			
-			int random1 = abs(buffer[k] % 255);
-			k++;
-			printf("processing: %d/75000\n",k);
-			int random2 = abs(buffer[k] % 255);
-			k++;
-			printf("processing: %d/75000\n",k);
-			int random3 = abs(buffer[k] % 255);
-			k++;
-			printf("processing: %d/75000\n",k);
-			
-			
-			rgb[0] = random1;
-			rgb[1] = random2;
-			rgb[2] = random3;
-			snprintf(line, sizeof(line), "%d %d %d\n", rgb[0], rgb[1], rgb[2]);
-			write(fd, line, strlen(line));
-		}
-			 
-	}
-	printf("done!\n");
 }
