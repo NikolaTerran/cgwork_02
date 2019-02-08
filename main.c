c3e3feef9c594609eb07d31228ec5871d4403108
Nikola Terran <nikolaterranthe1@gmail.com>
2019-02-08 12:30:06 -0500

    s

--- main.c
+++ main.c
@@ -26,34 +26,72 @@ int* sys_random(int size){
 void line_helper(int array[][500][3], int x1, int y1, int x2, int y2, int x3, int y3, int octants, int color[], double m){
 	
 	double slope;
-	//db();
+
 	while(x3 != x2 && y3 != y2){
+		slope = ((double)y1 - (double)y3)/((double)x3 - (double)x1);
 		switch(octants){
-			case 1: slope = (y1 - y3)/(x3 - x1);
+			case 1: 
 					if(slope > m){
-						db("slope",slope);
-					    db("y3",y3);
-						db("x3",x3);
+						//db("slope",slope);
+					    //db("y3",y3);
+						//db("x3",x3);
+						array[y3][x3][0] = color[0];
+						array[y3][x3][1] = color[1];
+						array[y3][x3][2] = color[2];
+						x3 ++;
+						//y3 --;
+					}else{
+					    //db("slope",slope);
+					    //db("y3",y3);
+						//db("x3",x3);
 						array[y3][x3][0] = color[0];
 						array[y3][x3][1] = color[1];
 						array[y3][x3][2] = color[2];
 						x3 ++;
 						y3 --;
-						//line_helper(array,x1,y1,x2,y2,x3 + 1,y3 + 1,octants,color,m);
-					}else{
-					    db("slope",slope);
-					    db("y3",y3);
-						db("x3",x3);
-						array[y3][x3 - 1][0] = color[0];
-						array[y3][x3 - 1][1] = color[1];
-						array[y3][x3 - 1][2] = color[2];
-						//line_helper(array,x1,y1,x2,y2,x3 + 1,y3,octants,color,m);
-						x3 ++;
 					}		
 			break;
 			case 2: //up right
+					if(slope > m){
+						db("slope",slope);
+						//db("y3",y3);
+						//db("x3",x3);
+						array[y3][x3][0] = color[0];
+						array[y3][x3][1] = color[1];
+						array[y3][x3][2] = color[2];
+						x3 ++;
+						y3 --;
+					}else{
+						db("slope",slope);
+						//db("y3",y3);
+						//db("x3",x3);
+						array[y3][x3][0] = color[0];
+						array[y3][x3][1] = color[1];
+						array[y3][x3][2] = color[2];
+						//x3 ++;
+						y3 --;
+					}		
 			break;
 			case 3: //up left
+					if(slope < m){
+						db("slope",slope);
+						//db("y3",y3);
+						//db("x3",x3);
+						array[y3][x3][0] = color[0];
+						array[y3][x3][1] = color[1];
+						array[y3][x3][2] = color[2];
+						//x3 --;
+						y3 --;
+					}else{
+						db("slope",slope);
+						//db("y3",y3);
+						//db("x3",x3);
+						array[y3][x3][0] = color[0];
+						array[y3][x3][1] = color[1];
+						array[y3][x3][2] = color[2];
+						x3 --;
+						y3 --;
+					}
 			break;
 			case 4: //left up
 			break;
@@ -78,7 +116,7 @@ int drawLine(int array[][500][3], int x1 , int y1, int x2, int y2, int color[]){
 				array[y1][x1][2] = color[2];
 	}else{
 				//{double m = 1;}
-			double m = (y2 - y1) / (x2 - x1);
+			double m = ((double)y1 - (double)y2) / ((double)x2 - (double)x1);
 			db("m",m);
 			
 				if(y2 <= y1){
@@ -119,9 +157,9 @@ int drawLine(int array[][500][3], int x1 , int y1, int x2, int y2, int color[]){
 				switch(octants){
 						case 1: line_helper(array,x1,y1,x2,y2,x1 + 1,y1 - 1,octants,color,m);
 						break;
-						case 2: //up right
+						case 2: line_helper(array,x1,y1,x2,y2,x1 + 1,y1 - 1,octants,color,m);
 						break;
-						case 3: //up left
+						case 3: line_helper(array,x1,y1,x2,y2,x1 - 1,y1 - 1,octants,color,m);
 						break;
 						case 4: //left up
 						break;
@@ -192,7 +230,15 @@ rgb[0] = 255;
 rgb[1] = 0;
 rgb[2] = 0;
 
-drawLine(array, 100, 100, 400, 50, rgb);
+//drawLine(array, 100, 100, 400, 50, rgb);
+//drawLine(array, 50, 100, 100, 50, rgb);
+//drawLine(array, 200, 300, 250, 30, rgb);
+//drawLine(array, 200, 100, 200, 0, rgb);
+drawLine(array, 250, 250, 50, 50, rgb);
+drawLine(array, 250, 250, 200, 30, rgb);
+//drawLine(array, 100, 100, 400, 50, rgb);
+//drawLine(array, 100, 100, 50, 50, rgb);
+//drawLine(array, 100, 100, 100, 50, rgb);
 
 push(array,fd);
 
