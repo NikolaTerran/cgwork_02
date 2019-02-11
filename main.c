d15e050583a9933d7dd5c7831d459ed73e53a2d3
Nikola Terran <nikolaterranthe1@gmail.com>
2019-02-11 11:41:59 -0500

    optimized the code

--- main.c
+++ main.c
@@ -116,94 +116,11 @@ void line_helper(int array[][500][3], int x1, int y1, int x2, int y2, int x3, in
 						x3 --;
 					}
 			break;
-			case 5: //left down
-					if(slope > m){
-						db("slope",slope);
-						//db("y3",y3);
-						//db("x3",x3);
-						array[y3][x3][0] = color[0];
-						array[y3][x3][1] = color[1];
-						array[y3][x3][2] = color[2];
-						x3 --;
-					}else{
-						db("slope",slope);
-						//db("y3",y3);
-						//db("x3",x3);
-						array[y3][x3][0] = color[0];
-						array[y3][x3][1] = color[1];
-						array[y3][x3][2] = color[2];
-						x3 --;
-						y3 ++;
-					}
-			break;
-			case 6: //down left
-					if(slope < m){
-						db("slope",slope);
-						//db("y3",y3);
-						//db("x3",x3);
-						array[y3][x3][0] = color[0];
-						array[y3][x3][1] = color[1];
-						array[y3][x3][2] = color[2];
-						y3 ++;
-					}else{
-						db("slope",slope);
-						//db("y3",y3);
-						//db("x3",x3);
-						array[y3][x3][0] = color[0];
-						array[y3][x3][1] = color[1];
-						array[y3][x3][2] = color[2];
-						x3 --;
-						y3 ++;
-					}
-			break;
-			case 7: //down right
-					if(slope > m){
-						db("slope",slope);
-						//db("y3",y3);
-						//db("x3",x3);
-						array[y3][x3][0] = color[0];
-						array[y3][x3][1] = color[1];
-						array[y3][x3][2] = color[2];
-						y3 ++;
-						
-					}else{
-						db("slope",slope);
-						//db("y3",y3);
-						//db("x3",x3);
-						array[y3][x3][0] = color[0];
-						array[y3][x3][1] = color[1];
-						array[y3][x3][2] = color[2];
-						x3 ++;
-						y3 ++;
-					}
-			break;
-			case 8: //right down
-					if(slope < m){
-						db("slope",slope);
-						//db("y3",y3);
-						//db("x3",x3);
-						array[y3][x3][0] = color[0];
-						array[y3][x3][1] = color[1];
-						array[y3][x3][2] = color[2];
-						x3 ++;
-					}else{
-						db("slope",slope);
-						//db("y3",y3);
-						//db("x3",x3);
-						array[y3][x3][0] = color[0];
-						array[y3][x3][1] = color[1];
-						array[y3][x3][2] = color[2];
-						x3 ++;
-						y3 ++;
-					}
-			break;
 		}
 	}
 }
 
 int drawLine(int array[][500][3], int x1 , int y1, int x2, int y2, int color[]){
-	int octants;
-	
 	if(x2 == x1 && y2 == y1){
 				array[y1][x1][0] = color[0];
 				array[y1][x1][1] = color[1];
@@ -213,60 +130,39 @@ int drawLine(int array[][500][3], int x1 , int y1, int x2, int y2, int color[]){
 			double m = ((double)y1 - (double)y2) / ((double)x2 - (double)x1);
 			db("m",m);
 			
+				array[y1][x1][0] = color[0];
+				array[y1][x1][1] = color[1];
+				array[y1][x1][2] = color[2];
+			
 				if(y2 <= y1){
 					if(x2 >= x1){
 						if( m <= 1 ){
-							 octants = 1;
+							 line_helper(array,x1,y1,x2,y2,x1 + 1,y1 - 1,1,color,m);
 						}else{
-							 octants = 2;
+							 line_helper(array,x1,y1,x2,y2,x1 + 1,y1 - 1,2,color,m);
 						}
 					}else{
 						if( m <= -1 ){
-							 octants = 3;
+							 line_helper(array,x1,y1,x2,y2,x1 - 1,y1 - 1,3,color,m);
 						}else{
-							 octants = 4;
+							 line_helper(array,x1,y1,x2,y2,x1 - 1,y1 - 1,4,color,m);
 						}
 					}
 				}else{
 				   if(x2 < x1){
 						if( m <= 1 ){
-							 octants = 5;
+							 line_helper(array,x2,y2,x1,y1,x2 + 1,y2 - 1,1,color,m);
 						}else{
-							 octants = 6;
+							 line_helper(array,x2,y2,x1,y1,x2 + 1,y2 - 1,2,color,m);
 						}
 					}else{
 						if( m <= -1 ){
-							 octants = 7;
+							 line_helper(array,x2,y2,x1,y1,x2 - 1,y2 - 1,3,color,m);
 						}else{
-							 octants = 8;
+							 line_helper(array,x2,y2,x1,y1,x2 - 1,y2 - 1,4,color,m);
 						}
 					}
 				}
-				
-				db("octants",(double)octants);
-				
-				array[y1][x1][0] = color[0];
-				array[y1][x1][1] = color[1];
-				array[y1][x1][2] = color[2];
-				db("lol",(double)octants);
-				switch(octants){
-						case 1: line_helper(array,x1,y1,x2,y2,x1 + 1,y1 - 1,octants,color,m);
-						break;
-						case 2: line_helper(array,x1,y1,x2,y2,x1 + 1,y1 - 1,octants,color,m);
-						break;
-						case 3: line_helper(array,x1,y1,x2,y2,x1 - 1,y1 - 1,octants,color,m);
-						break;
-						case 4: line_helper(array,x1,y1,x2,y2,x1 - 1,y1 - 1,octants,color,m);//left up
-						break;
-						case 5: line_helper(array,x1,y1,x2,y2,x1 - 1,y1 + 1,octants,color,m);//left down
-						break;
-						case 6: line_helper(array,x1,y1,x2,y2,x1 - 1,y1 + 1,octants,color,m);//down left
-						break;
-						case 7: line_helper(array,x1,y1,x2,y2,x1 + 1,y1 + 1,octants,color,m);//down right
-						break;
-						case 8: line_helper(array,x1,y1,x2,y2,x1 + 1,y1 + 1,octants,color,m);// right down
-						break;
-						}
 	}
 }
 
